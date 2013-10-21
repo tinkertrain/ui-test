f9cfbb26779fe949734343f0b6ed7a3b864d517a
Pablo Lomeli <pablo@mustard-designagency.com>
2013-10-20 23:46:53 -0500

    Convert to factory function, added API, you can now pass which section has the 'read more' functionality and the height it should be when collapsed

--- public/js/my-read-more.js
+++ public/js/my-read-more.js
@@ -1,38 +1,40 @@
 (function(){
-  var readMore = {
-    $section : $('section[role="main"]'),
-    $button : $('<button class="readmore">Read More</button>'),
 
-    initialize : function() {
-      var sectionHeight = this.$section.height();
-      this.insertButton();
-      this.$section.height(400).addClass('readingAllShadow');
-      this.showHide(sectionHeight);
-    },
-
-    insertButton : function() {
-      this.$button.insertAfter(this.$section);
-    },
-
-    showHide : function(sectionHeight) {
-      var _this = this;
-      this.$button.on('click', function(e) {
-        e.preventDefault();
-        if(_this.$section.height() === 400) {
-          _this.$section.height(sectionHeight);
-          _this.$section.removeClass('readingAllShadow');
-          _this.$button.text('Read Less');
-        }
-        else {
-          _this.$section.height(400);
-          _this.$section.addClass('readingAllShadow');
-          _this.$button.text('Read More');
-        }
-
-      });
-    }
+  var ReadMore = function(section, readMoreHeight){
+    this.$section = $(section);
+    this.$button = $('<button class="readmore">Read More</button>');
+    this.readMoreHeight = readMoreHeight;
+    this.initialize();
   };
 
-  readMore.initialize();
+  ReadMore.prototype.initialize = function() {
+    var sectionHeight = this.$section.height();
+    this.insertButton();
+    this.$section.height(this.readMoreHeight).addClass('readingAllShadow');
+    this.showHide(sectionHeight);
+  };
+
+  ReadMore.prototype.insertButton = function() {
+    this.$button.insertAfter(this.$section);
+  };
+
+  ReadMore.prototype.showHide = function(sectionHeight) {
+    var _this = this;
+    this.$button.on('click', function(e) {
+      e.preventDefault();
+      if(_this.$section.height() === this.readMoreHeight) {
+        _this.$section.height(sectionHeight);
+        _this.$section.removeClass('readingAllShadow');
+        _this.$button.text('Read Less');
+      }
+      else {
+        _this.$section.height(this.readMoreHeight);
+        _this.$section.addClass('readingAllShadow');
+        _this.$button.text('Read More');
+      }
+    });
+  };
+
+  var readMore = new ReadMore('section[role="main"]', 400);
 
 })();
